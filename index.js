75588461d8a984c36db315231f858fc2f2c7dd86
Malek Haj-Hussein <malekadair@gmail.com>
2019-09-28 08:56:05 -0500

    graybox wireframing completed.

---
+++ index.js
@@ -0,0 +1,26 @@
+// $(() => {
+//     $('.js-start-quiz').on('click', function(){
+//         $('.js-first').attr('class','hidden')
+//         $('.js-second').removeAttr('class','hidden')
+//     })
+
+
+//     $('.js-form').submit( function(e){
+//         e.preventDefault()
+
+//         $('.js-second').attr('class','hidden')
+//         $('.js-third').removeAttr('class','hidden')
+//     })
+
+
+//     $('.js-next-question').on('click', function(){
+//         $('.js-third').attr('class','hidden')
+//         $('.js-fourth').removeAttr('class','hidden')
+//     })
+
+
+//     $('.js-start-over').on('click', function(){
+//         $('.js-fourth').attr('class','hidden')
+//         $('.js-first').removeAttr('class','hidden')
+//     })
+// })
