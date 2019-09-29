25233185f6078581b7b2d8b60044df56e65acf41
Malek Haj-Hussein <malekadair@gmail.com>
2019-09-29 09:02:54 -0500

    actually added questions this time

---
+++ questions.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+        question: "When was the first game of billiards played?",
+        answers: {
+            "1964": false,
+            "200 BC": false,
+            "200 AD": false,
+            "15th Century": true,
+        }
+    },
+    {
+        question: "Because Billiards was once illegal in Virginia, this former president built his home to conceal a billiards room specifically to play without worry of prosecution.",
+        answers: {
+            "Thomas Jefferson": true,
+            "George Washington": false,
+            "Samuel Adams": false,
+            "Andrew Jackson": false,
+        }
+    },
+    {
+        question: "What's another name for a pool stick?",
+        answers: {
+            "Wand": false,
+            "Pusher": false,
+            "Shooty-thingy": false,
+            "Cue": true,
+        }
+    },
+    {
+        question: 'What is the main reason that someone would be called a "hustler" in pool?',
+        answers: {
+            "They put in a lot of time practicing": false,
+            "They pretend to be unskilled at the game in order to get people to gamble with them": true,
+            "They sweat profusely while they play": false,
+            "They shoot their shots very quickly": false,
+        }
+    },
+    {
+        question: "Which of the following is NOT a type of pool/billiards",
+        answers: {
+            "3 cushion billiards": false,
+            "8-ball": false,
+            "Sraight pool": false,
+            "Dog's Pocket pool": true,
+        }
+    },
+    {
+        question: "What is another name for committing a foul in most games of pool?",
+        answers: {
+            "Breaking": false,
+            "Banking": false,
+            "Scratching": true,
+            "Scraping": false,
+        }
+    },
+    {
+        question: "Who holds the (unofficial) record for the fastest Maximum Break in Snooker at a time of 5 minutes and 20 seconds?",
+        answers: {
+            "Ronnie O'Sullivan": true,
+            "Paul Newman": false,
+            "Malek Haj-Hussein": false,
+            "Shane Van Boening": false,
+        }
+    },
+    {
+        question: "",
+        answers: {
+            "": false,
+            "": true,
+            "": false,
+            "": false,
+        }
+    },
+    {
+        question: "",
+        answers: {
+            "": false,
+            "": true,
+            "": false,
+            "": false,
+        }
+    },
+    {
+        question: "",
+        answers: {
+            "": false,
+            "": true,
+            "": false,
+            "": false,
+        }
+    }
+]
