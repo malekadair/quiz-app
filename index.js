c4f38da18636384702e79d13b734b19e58a13663
Malek Haj-Hussein <malekadair@gmail.com>
2019-09-29 17:31:49 -0500

    1 day's worth of work. Completed almost all except small bugs and styling.

--- index.js
+++ index.js
@@ -4,11 +4,37 @@ let questionIndex = 0;
 let currentView;
 
 
+function anyButtonListen(){
+    $('main').on('click','.js-btn', function(e){
+        e.preventDefault()
+
+        if (currentView === 'start'){
+            loadQuestion()
+        } else if (currentView === 'question') {
+            checkAnswerCorrect()
+            if (currentView === 'correct'){
+                giveFeedback(true)
+            } else {
+                giveFeedback(false)
+            }
+        } else if (currentView === 'feedback') {
+            questionIndex === QUESTIONS.length 
+                ? loadResults() 
+                : loadQuestion();
+        } else {
+            loadStart()
+            correctCount = 0;
+            incorrectCount = 0;
+            questionIndex = 0;
+        }
+    })
+}
+
 function loadStart () {
     currentView = 'start';
 
     $('main').html(`
-    <section class="js-start" data-page="start">
+    <section class="js-start container" data-page="start">
         <h2>Are you a Pool-Shark?</h2>
         <p>In this quiz, we're going to test your knowledge on the game of Billiards.</p>
         <p>Are you ready to get started?</p>
@@ -17,76 +43,106 @@ function loadStart () {
     
 }
 
-function buttonListen(){
-    $('main').on('click','.js-btn', function(e){
-        // if (typeof e.cancelable !== 'boolean' || e.cancelable) {
-        //     // The event can be canceled, so we do so.
-        //     e.preventDefault();
-        loadQuestion()
-    })
-}
-
 function loadQuestion () {
     currentView = 'question'
-    
 
-    // alert('load question')
-    // $('main').html(`<p>we did it</p>`)
-    $('main').html(`<section class="js-question" data-page="question">
+    $('main').html(`
+<section class="js-question container" data-page="question">
     <form class="js-form" data-page="question">
         <fieldset>
-            <legend>Question #${questionIndex + 1} </legend>
-            <div>
-                <input type="radio" name="answer" id="ans1" value="answer1"/>
-                <label for="answer1">answer<label>
+            <legend>Question #${questionIndex + 1} - ${QUESTIONS[questionIndex].question} </legend>
+            <div class="${QUESTIONS[questionIndex].ANSWERS[0].correct}">
+                <input type="radio" name="answer" id="ans1" value="${QUESTIONS[questionIndex].ANSWERS[0].correct}"/>
+                <label for="ans1">${QUESTIONS[questionIndex].ANSWERS[0].ans}<label>
             </div>
-            <div>
-                <input type="radio" name="answer" id="ans2" value="answer2"/>
-                <label for="answer2">answer<label>
+            <div class="${QUESTIONS[questionIndex].ANSWERS[1].correct}">
+                <input type="radio" name="answer" id="ans2" value="${QUESTIONS[questionIndex].ANSWERS[1].correct}"/>
+                <label for="ans2">${QUESTIONS[questionIndex].ANSWERS[1].ans}<label>
             </div>
-            <div>
-                <input type="radio" name="answer" id="ans3" value="answer3"/>
-                <label for="answer3">answer<label>
+            <div class="${QUESTIONS[questionIndex].ANSWERS[2].correct}">
+                <input type="radio" name="answer" id="ans3" value="${QUESTIONS[questionIndex].ANSWERS[2].correct}"/>
+                <label for="ans3">${QUESTIONS[questionIndex].ANSWERS[2].ans}<label>
             </div>
-            <div>
-                <input type="radio" name="answer" id="ans4" value="answer4"/>
-                <label for="answer4">answer<label>
+            <div class="${QUESTIONS[questionIndex].ANSWERS[3].correct}">
+                <input type="radio" name="answer" id="ans4" value="${QUESTIONS[questionIndex].ANSWERS[3].correct}"/>
+                <label for="ans4">${QUESTIONS[questionIndex].ANSWERS[3].ans}<label>
             </div>
-            <button type="submit" class="btn js-submit">Submit</button>
+            <button type="submit" class="js-btn js-submit">Submit</button>
         </fieldset>
     </form>
 </section>
-<footer>
+<footer class="container">
     <h3 class="score">Your score:</h3>
-    <p>6 correct, 3 incorrect</p>
+    <p>${correctCount} correct, ${incorrectCount} incorrect</p>
 </footer>`)
 }
 
 function checkAnswerCorrect(){
 //check to see if user's input is correct or not by taking it's 
+    let answerVal = $('input[name=answer]:checked').val()
+    if (!answerVal){
+        alert('Must make a selection.')
+    } else if (answerVal === "true"){
+        currentView = 'correct'
+        let trueOrFalse = true;
+        updateScore(trueOrFalse)
+    } else{
+        currentView = 'incorrect'
+        let trueOrFalse = false;
+        updateScore(trueOrFalse)
+    }
 }
-function giveFeedback(){
-    checkAnswerCorrect();
+function giveFeedback(isCorrect){
 //call checkAnswerCorrect() function to find out whether or not user was correct
 //if the answer was correct, highlight user's answer in green box with light green background and tell user "correct!". Then add 1 to correctCount variable by calling updateScore (true)
-//if the answer was incorrect,highlight user's answer in red box with light red background and tell user "sorry, that's incorrect!" and highlight correct answer in green box with light green background. Then add 1 to incorrectCount variable by calling updateScore (false)
+//if the answer was incorrect,highlight user's answer in red box with light red background and tell user "sorry, that is wrong!" and highlight correct answer in green box with light green background. Then add 1 to incorrectCount variable by calling updateScore (false)
+
+    currentView = 'feedback'
+    
+    if (isCorrect){
+        $('div .true').css('background-color', '#baffba').css('border', 'green solid 2px').css('border-radius', '4px')
+        $('div .true').append('<p class="pCorrect">Correct!</p>')
+    } else {
+        $('input[name=answer]:checked').closest('div').css('background-color', '#ffcbc7').css('border', 'red solid 2px').css('border-radius', '4px')
+        $('input[name=answer]:checked').closest('div').append('<p class="pIncorrect">Sorry, that is Wrong.</p>')
+
+        $('div .true').css('background-color', '#baffba').css('border', 'green solid 2px').css('border-radius', '4px')
+        $('div .true').append('<p class="pCorrect">This is the Correct Answer.</p>')
+        
+    }
+    $('div .true').css('background-color', '#baffba').css('border', 'green solid 2px').css('border-radius', '4px')
+
+    if (questionIndex == QUESTIONS.length -1){
+        $('.js-form button').text('See Results')
+    } else{
+        $('.js-form button').text('Next Question')
+    }
+
+    
+    updateQuestionIndex()
 }
 function updateScore(isCorrect){
 //if isCorrect = true, correct++ else, incorrect++
+    isCorrect ? correctCount++ : incorrectCount++;
+    $('footer').html(`<h3 class="score">Your score:</h3>
+    <p>${correctCount} correct, ${incorrectCount} incorrect</p>`)
 }
 function updateQuestionIndex(){
-//questionIndex++
+    questionIndex++;
 }
 function loadResults(){
+    currentView = 'results'
     $('main').html(`
-    <section class="js-results" data-page="finish" >
+    <section class="js-results container" data-page="finish" >
         <h2>You've completed the quiz!</h2>
         <p>You got ${correctCount} out of 10 questions correct!</p>
         <p>Do you want to try again?</p>
-        <button class="btn js-start-over">Start Over</button>
+        <button class="js-btn js-start-over">Start Over</button>
     </section>`)
 }
 $(() => {
     loadStart();
-    buttonListen();
+    anyButtonListen();
 })
+
+// loadStart>loadQuestion>checkAnswerCorrect>giveFeedback>load next question or loadResults
--- questions.js
+++ questions.js
@@ -1,92 +1,92 @@
-const questions = [
+const QUESTIONS = [
     {
         question: "When was the first game of billiards played?",
-        answers: {
-            "1964": false,
-            "200 BC": false,
-            "200 AD": false,
-            "15th Century": true,
-        }
+        ANSWERS: [
+            {ans: "1964", correct: false},
+            {ans: "200 BC", correct: false},
+            {ans: "200 AD", correct: false},
+            {ans: "15th Century", correct: true},
+        ]
     },
     {
         question: "Because Billiards was once illegal in Virginia, this former president built his home to conceal a billiards room specifically to play without worry of prosecution.",
-        answers: {
-            "Thomas Jefferson": true,
-            "George Washington": false,
-            "Samuel Adams": false,
-            "Andrew Jackson": false,
-        }
+        ANSWERS: [
+            {ans: "Thomas Jefferson", correct: true},
+            {ans: "George Washington", correct: false},
+            {ans: "Samuel Adams", correct: false},
+            {ans: "Andrew Jackson", correct: false},
+        ]
     },
     {
         question: "What's another name for a pool stick?",
-        answers: {
-            "Wand": false,
-            "Pusher": false,
-            "Shooty-thingy": false,
-            "Cue": true,
-        }
+        ANSWERS: [
+            {ans: "Wand", correct: false},
+            {ans: "Pusher", correct: false},
+            {ans: "Shooty-thingy", correct: false},
+            {ans: "Cue", correct: true},
+        ]
     },
     {
         question: 'What is the main reason that someone would be called a "hustler" in pool?',
-        answers: {
-            "They put in a lot of time practicing": false,
-            "They pretend to be unskilled at the game in order to get people to gamble with them": true,
-            "They sweat profusely while they play": false,
-            "They shoot their shots very quickly": false,
-        }
+        ANSWERS: [
+            {ans: "They put in a lot of time practicing", correct: false},
+            {ans: "They pretend to be unskilled at the game in order to get people to gamble with them", correct: true},
+            {ans: "They sweat profusely while they play", correct: false},
+            {ans:"They shoot their shots very quickly", correct: false},
+        ]
     },
     {
         question: "Which of the following is NOT a type of pool/billiards",
-        answers: {
-            "3 cushion billiards": false,
-            "8-ball": false,
-            "Sraight pool": false,
-            "Dog's Pocket pool": true,
-        }
+        ANSWERS: [
+            {ans: "3 cushion billiards", correct: false},
+            {ans: "8-ball", correct: false},
+            {ans: "Sraight pool", correct: false},
+            {ans: "Dog's Pocket pool", correct: true},
+        ]
     },
     {
         question: "What is another name for committing a foul in most games of pool?",
-        answers: {
-            "Breaking": false,
-            "Banking": false,
-            "Scratching": true,
-            "Scraping": false,
-        }
+        ANSWERS: [
+            {ans: "Breaking", correct: false},
+            {ans: "Banking", correct: false},
+            {ans: "Scratching", correct: true},
+            {ans: "Scraping", correct: false},
+        ]
     },
     {
         question: "Who holds the (unofficial) record for the fastest Maximum Break in Snooker at a time of 5 minutes and 20 seconds?",
-        answers: {
-            "Ronnie O'Sullivan": true,
-            "Paul Newman": false,
-            "Malek Haj-Hussein": false,
-            "Shane Van Boening": false,
-        }
+        ANSWERS: [
+            {ans: "Ronnie O'Sullivan", correct: true},
+            {ans: "Paul Newman", correct: false},
+            {ans: "Malek Haj-Hussein", correct: false},
+            {ans: "Shane Van Boening", correct: false},
+        ]
     },
     {
         question: "",
-        answers: {
-            "": false,
-            "": true,
-            "": false,
-            "": false,
-        }
+        ANSWERS: [
+            {ans: "", correct: false},
+            {ans: "", correct: true},
+            {ans: "", correct: false},
+            {ans: "", correct: false},
+        ]
     },
     {
         question: "",
-        answers: {
-            "": false,
-            "": true,
-            "": false,
-            "": false,
-        }
+        ANSWERS: [
+            {ans: "", correct: false},
+            {ans: "", correct: true},
+            {ans: "", correct: false},
+            {ans: "", correct: false},
+        ]
     },
     {
         question: "",
-        answers: {
-            "": false,
-            "": true,
-            "": false,
-            "": false,
-        }
+        ANSWERS: [
+            {ans: "", correct: false},
+            {ans: "", correct: true},
+            {ans: "", correct: false},
+            {ans: "", correct: false},
+        ]
     }
 ]
